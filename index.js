d3984d6cef2681611ceb3f96a42bd749804e12c5
krisko15k-web <81244898+krisko15k-web@users.noreply.github.com>
2022-10-24 22:23:24 +0300

    Adding the Input Generatior

---
+++ index.js
@@ -0,0 +1,17 @@
+let inputCont = []
+
+let inputWord
+let randWord = document.getElementById("randWord")
+let pickedWord
+
+function addInput() {
+    inputWord = document.getElementById("inputWord").value
+    inputCont.push(inputWord)
+}
+
+function randPick() {
+    for(let i = 0; i < inputCont.length; i++) {
+        pickedWord = Math.floor(Math.random() * inputCont.length)
+        randWord.textContent = inputCont[pickedWord]
+    }
+}
